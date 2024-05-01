5dac0a9994345ec652493ac5ea117707aad05810
chaiheung <chaiheung@naver.com>
2024-05-01 10:45:57 +0900

    게시물 작성

---
+++ src/main/java/com/caprj1/service/BoardService.java
@@ -0,0 +1,18 @@
+package com.caprj1.service;
+
+import com.caprj1.domain.Board;
+import com.caprj1.mapper.BoardMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(rollbackFor = Exception.class)
+public class BoardService {
+    private final BoardMapper mapper;
+
+    public void add(Board board) {
+        mapper.insert(board);
+    }
+}
