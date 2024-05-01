1e2a01d0582c8edcadad5aa2719bc323896b1f7d
chaiheung <chaiheung@naver.com>
2024-05-01 09:24:30 +0900

    first commit

---
+++ src/main/java/com/caprj1/CaPrj1Application.java
@@ -0,0 +1,13 @@
+package com.caprj1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaPrj1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaPrj1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/caprj1/CaPrj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caprj1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaPrj1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
