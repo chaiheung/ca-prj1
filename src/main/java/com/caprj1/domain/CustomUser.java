855ce002e124e6d22ff53a48e647d83d3083bd91
chaiheung <chaiheung@naver.com>
2024-05-07 10:16:04 +0900

    권한

--- src/main/java/com/caprj1/domain/CustomUser.java
+++ src/main/java/com/caprj1/domain/CustomUser.java
@@ -4,16 +4,17 @@ import lombok.Getter;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
-import java.util.List;
-
 @Getter
 public class CustomUser extends User {
     private Member member;
 
     public CustomUser(Member member) {
-        super(member.getEmail(), member.getPassword(), List.of(
-                new SimpleGrantedAuthority("admin")
-        ));
+        super(member.getEmail(),
+                member.getPassword(),
+                member.getAuthority()
+                        .stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .toList());
         this.member = member;
     }
 }
